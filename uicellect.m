f499719a7a1c6b75309cae3b133fb6699eb4e117
Bob Spunt <bobspunt@gmail.com>
2017-06-22 12:39:00 -0700

    when only 1 item chosen, output is converted to CHAR

--- uicellect.m
+++ uicellect.m
@@ -6,32 +6,32 @@ function [theChosen, theChosenIDX] = uicellect(theCell, varargin)
 % ________________________________________________________________________________________
 % OUTPUTS
 %
-%	  theChosen:     cell array of chosen items (empty if none chosen)
-%	  theChosenIDX:  idx to input cell array of choices
+%	  theChosen:     chosen items. cell array unless N chosen is 1 (CHAR) or 0 (empty array)
+%	  theChosenIDX:  numeric idx to input cell array of choices
 %
 % ________________________________________________________________________________________
 % INPUTS
 %
-%	  theCell:  cell array of items to choose from
+%	  theCell:  cell array of items to choose from. cell contents must be type CHAR.
 %
 % ________________________________________________________________________________________
 % VARARGIN (partial matches OK; run without arguments to see default values)
 %
-% | NAME            | DEFAULT       
+% | NAME            | DEFAULT
 % |-----------------|---------------------------------------------------------------------
 % | DefaultIDX      | optional indices to inCell for items selected by default
 % | Prompt          | message to present to user at top of gui
-% | StripPath       | if true (1) and inputs are paths to files, strips path from items 
-% | MultiSelect     | if true (1), user allowed to select multiple items           
+% | StripPath       | if true (1) and inputs are paths to files, strips path from items
+% | MultiSelect     | if true (1), user allowed to select multiple items
 % | MaxPerColumn    | max items per column (if > # of items, then one column layout)
-% | RowPixelHeight  | height of gui rows (one item per row/column), in pixels     
-% | ColPixelWidth   | width of gui columns, in pixels        
-% | BaseFontSize    | base font size (used for item labels)  
+% | RowPixelHeight  | height of gui rows (one item per row/column), in pixels
+% | ColPixelWidth   | width of gui columns, in pixels
+% | BaseFontSize    | base font size (used for item labels)
 % | hAlign          | gui horizontal alignment, can be: middle,top,upper,bottom,lower
 % | vAlign          | gui vertical alignment, can be: center, left, right
-% | BackgroundColor | gui background color             
+% | BackgroundColor | gui background color
 % | ForegroundColor | gui foreground color
-% | ItemBackgroundColor | item background color             
+% | ItemBackgroundColor | item background color
 % | ItemForegroundColor | item foreground color
 %
 % ________________________________________________________________________________________
@@ -41,19 +41,19 @@ function [theChosen, theChosenIDX] = uicellect(theCell, varargin)
 %   theCell = cellfun(@sprintf,repmat({'Item %d'},25,1), num2cell((1:25)'),'Unif',false);
 %
 %   % - Present in GUI using Default Settings
-%   [theChosen, theChosenIDX] = uicellect(theCell); 
+%   [theChosen, theChosenIDX] = uicellect(theCell);
 %
 %   % - Present in GUI and disable Multi-Selection
-%   [theChosen, theChosenIDX] = uicellect(theCell, 'Multi', 0); 
+%   [theChosen, theChosenIDX] = uicellect(theCell, 'Multi', 0);
 %
 %   % - Present in GUI but Change How it Looks
-%   [theChosen, theChosenIDX] = uicellect(theCell,'MaxPer',15,'RowPix',35,'ColPix',150); 
+%   [theChosen, theChosenIDX] = uicellect(theCell,'MaxPer',15,'RowPix',35,'ColPix',150);
 %
 
 % ----------------------------- Copyright (C) 2015 Bob Spunt -----------------------------
 %	Created:  2015-08-23
 %	Email:     spunt@caltech.edu
-% 
+%
 %   This program is free software: you can redistribute it and/or modify
 %   it under the terms of the GNU General Public License as published by
 %   the Free Software Foundation, either version 3 of the License, or (at
@@ -67,7 +67,7 @@ function [theChosen, theChosenIDX] = uicellect(theCell, varargin)
 % ________________________________________________________________________________________
 
 % | DEFAULTS FOR VARARGIN
-def = { ... 
+def = { ...
       'DefaultIDX',       [], ...
       'Prompt',           'Select from:',	...
       'StripPath',        true, ...
@@ -99,16 +99,16 @@ if all(cellfun(@exist, theCell, repmat({'file'}, nopt, 1))) && StripPath
     [theCellPath, n, e] = cellfun(@fileparts, theCell, 'Unif', false);
     theCell             = strcat(n, e);
 else
-    StripPath = 0; 
+    StripPath = 0;
 end
 
 % | CALCULATE GRID
 if nopt > MaxPerColumn
     ncol = ceil(nopt/MaxPerColumn);
-    nrow = ceil(nopt/ncol); 
+    nrow = ceil(nopt/ncol);
 else
-    ncol = 1; 
-    nrow = nopt; 
+    ncol = 1;
+    nrow = nopt;
 end
 
 % | CALCULATE FIGURE POSITION
@@ -130,7 +130,7 @@ fige = figure( ...
             'Tag'          , 'uicellect      dialogue',...
             'WindowStyle'  , 'normal'        ,...
             'UserData'     , 1);
-                    
+
 % | USE PGRID TO CREATE UIPANEL LAYOUT
 pbase = pgrid(3, 1, ...
             'margin',    .01              ,...
@@ -140,7 +140,7 @@ pbase = pgrid(3, 1, ...
             'backg',     BackgroundColor  ,...
             'foreg',     ForegroundColor);
 set(pbase(2), 'BackgroundColor', ItemBackgroundColor, 'ForegroundColor', ItemForegroundColor);
-        
+
 % | UPPER UIPANEL:  PROMPT
 ht  =  uicontrol( ...
              'Style'               , 'text'            ,...
@@ -162,17 +162,17 @@ ht  =  uicontrol( ...
 % | MIDDLE UIPANELS:  CELL ITEMS
 [popt, pidx] = pgrid(nrow, ncol, 'parent', pbase(2), 'backg', ItemBackgroundColor, 'foreg', ItemForegroundColor);
 
-% | RE-ARRANGE SO THAT COLS ARE TRAVERSED FIRST 
+% | RE-ARRANGE SO THAT COLS ARE TRAVERSED FIRST
 pidx(:,3) = 1:length(popt);
-pidx      = sortrows(pidx,2); 
-popt      = popt(pidx(:,3)); 
+pidx      = sortrows(pidx,2);
+popt      = popt(pidx(:,3));
 set(pbase(2), 'bordertype', 'line');
 
 % | CREATE UICONTROL FOR EACH CELL
 if MultiSelect
     theItemCallback = '';
 else
-    theItemCallback = {@cb_changeselection, pbase(2)};  
+    theItemCallback = {@cb_changeselection, pbase(2)};
 end
 isSelected = zeros(nopt, 1);
 if DefaultIDX, isSelected(DefaultIDX) = 1; end
@@ -198,13 +198,13 @@ for i = 1:nopt
               'FontWeight'   ,     'normal'                               ,...
                 'UserData'   ,     MultiSelect                            ,...
                  'Visible'   ,     'on');
-     drawnow; 
+     drawnow;
 end
 
 
-     
+
 % | LOWER UIPANELS:  PUSH BUTTONS
-pui = pgrid(1, ncol+1, 'parent', pbase(3), 'backg', BackgroundColor, 'foreg', ForegroundColor); 
+pui = pgrid(1, ncol+1, 'parent', pbase(3), 'backg', BackgroundColor, 'foreg', ForegroundColor);
 
 % | Select All
 if MultiSelect
@@ -255,7 +255,7 @@ hok  =  uicontrol( ...
                     );
 
 % | FINISH UP
-set(findall(fige, '-property', 'FontName'), 'FontName', FontName); 
+set(findall(fige, '-property', 'FontName'), 'FontName', FontName);
 set(fige, 'CloseRequestFcn', {@cb_closefig, fige, 0})
 if MultiSelect, set(htog, 'Callback', {@cb_selectall, opth}); end
 set(hok, 'Callback', {@cb_closefig, fige, 1})
@@ -269,6 +269,7 @@ if ishandle(fige)
         idx = cell2mat(get(opth, 'Value'));
         if any(idx)
             theChosen       = theCell(find(idx));
+            if length(theChosen)==1, theChosen = char(theChosen); end
             theChosenIDX    = find(idx);
         end
     end
@@ -294,16 +295,16 @@ function [ph, pidx] = pgrid(nrow, ncol, varargin)
 %   ncol:   number of cols in grid
 % ________________________________________________________________________________________
 %  VARARGIN
-% | NAME            | DEFAULT       | DESCRIPTION 
+% | NAME            | DEFAULT       | DESCRIPTION
 % |-----------------|---------------|-----------------------------------------------------
-% | parent          | gcf           | parent object for grid 
-% | relwidth        | ones(1, ncol) | relative width of columns (arbitrary units)            
-% | relheight       | ones(1, nrow) | relative height of rows (arbitrary units)            
-% | marginsep       | 0.0100        | size of margin surrounding grid (normalized units)           
-% | panelsep        | 0.0100        | size of space between panels (normalized units)            
-% | backgroundcolor | [.08 .09 .09] | uipanel background color             
+% | parent          | gcf           | parent object for grid
+% | relwidth        | ones(1, ncol) | relative width of columns (arbitrary units)
+% | relheight       | ones(1, nrow) | relative height of rows (arbitrary units)
+% | marginsep       | 0.0100        | size of margin surrounding grid (normalized units)
+% | panelsep        | 0.0100        | size of space between panels (normalized units)
+% | backgroundcolor | [.08 .09 .09] | uipanel background color
 % | foregroundColor | [.97 .97 .97] | uipanel foreground color
-% | bordertype      | 'none'        | etchedin, etchedout, beveledin, beveledout, line 
+% | bordertype      | 'none'        | etchedin, etchedout, beveledin, beveledout, line
 % | borderwidth     | 1             | uipanel border width in pixels
 % ________________________________________________________________________________________
 %
@@ -337,7 +338,7 @@ if isempty(relheight), relheight    = ones(1, nrow); end
 if length(relwidth)~=ncol, printmsg('Length of RELWIDTH must equal NCOL. Try again!'); ph = []; pidx = []; return; end
 if length(relheight)~=nrow, printmsg('Length of RELHEIGHT must equal NROW. Try again!'); ph = []; pidx = []; return; end
 
-% | Get normalized positions for each panel 
+% | Get normalized positions for each panel
 pos         = getpositions(relwidth, relheight, marginsep, panelsep);
 pidx        = pos(:,1:2);
 hpos        = pos(:,3:end);
@@ -374,14 +375,14 @@ if nargin<5, top2bottomidx = 1; end
 if size(relheight,1) > 1, relheight = relheight'; end
 if size(relwidth, 1) > 1, relwidth = relwidth'; end
 ncol        = length(relwidth);
-nrow        = length(relheight); 
+nrow        = length(relheight);
 if top2bottomidx, relheight = relheight(end:-1:1); end
 
 % width
-rowwidth    = 1-(marginsep*2)-(uicontrolsep*(ncol-1));  
+rowwidth    = 1-(marginsep*2)-(uicontrolsep*(ncol-1));
 uiwidths    = (relwidth/sum(relwidth))*rowwidth;
 allsep      = [marginsep repmat(uicontrolsep, 1, ncol-1)];
-uilefts     = ([0 cumsum(uiwidths(1:end-1))]) + cumsum(allsep); 
+uilefts     = ([0 cumsum(uiwidths(1:end-1))]) + cumsum(allsep);
 
 % height
 colheight   = 1-(marginsep*2)-(uicontrolsep*(nrow-1));
@@ -395,9 +396,9 @@ if top2bottomidx, uibottoms = uibottoms(end:-1:1); end
 pos = zeros(ncol*nrow, 6);
 pos(:,1) = reshape(repmat(nrow:-1:1, ncol, 1), size(pos,1), 1);
 pos(:,2) = reshape(repmat(1:ncol, 1, nrow), size(pos,1), 1);
-pos(:,3) = uilefts(pos(:,2)); 
-pos(:,4) = uibottoms(pos(:,1)); 
-pos(:,5) = uiwidths(pos(:,2)); 
+pos(:,3) = uilefts(pos(:,2));
+pos(:,4) = uibottoms(pos(:,1));
+pos(:,5) = uiwidths(pos(:,2));
 pos(:,6) = uiheights(pos(:,1));
 pos      = sortrows(pos, 1);
 end
@@ -405,10 +406,10 @@ function argstruct  = setargs(defaults, optargs)
 % SETARGS Name/value parsing and assignment of varargin with default values
 if nargin < 1, mfile_showhelp; return; end
 if nargin < 2, optargs = []; end
-defaults = reshape(defaults, 2, length(defaults)/2)'; 
+defaults = reshape(defaults, 2, length(defaults)/2)';
 if ~isempty(optargs)
     if mod(length(optargs), 2)
-        error('Optional inputs must be entered as Name, Value pairs, e.g., myfunction(''name'', value)'); 
+        error('Optional inputs must be entered as Name, Value pairs, e.g., myfunction(''name'', value)');
     end
     arg = reshape(optargs, 2, length(optargs)/2)';
     for i = 1:size(arg,1)
@@ -419,7 +420,7 @@ if ~isempty(optargs)
            error('Input "%s" does not match a valid input.', arg{i,1});
        else
            defaults{idx,2} = arg{i,2};
-       end  
+       end
     end
 end
 for i = 1:size(defaults,1), assignin('caller', defaults{i,1}, defaults{i,2}); end
@@ -434,9 +435,9 @@ function figpos     = align_figure(uiW, uiH, vAlign, hAlign)
         case 'middle'
           figpos(2) = (screenH/2)-(uiH/2);
         case {'top', 'upper'}
-          figpos(2) = screenH-uiH; 
+          figpos(2) = screenH-uiH;
         case {'bottom', 'lower'}
-          figpos(2) = 1; 
+          figpos(2) = 1;
         otherwise
           error('vAlign options are: middle, top, upper, bottom, lower')
     end
@@ -444,9 +445,9 @@ function figpos     = align_figure(uiW, uiH, vAlign, hAlign)
         case 'center'
           figpos(1) = (screenW/2) - (uiW/2);
         case 'right'
-          figpos(1) = screenW - uiW; 
+          figpos(1) = screenW - uiW;
         case 'left'
-          figpos(1) = 1; 
+          figpos(1) = 1;
         otherwise
           error('hAlign options are: center, left, right')
     end
@@ -461,16 +462,16 @@ function cb_selectall(varargin)
     idx = strcmp(str, get(varargin{1}, 'String'));
     for i = 1:length(h)
         set(h, 'Value', find(idx)-1);
-        drawnow; 
+        drawnow;
     end
     set(varargin{1}, 'String', str(~idx));
-    drawnow;   
+    drawnow;
 end
 function cb_changeselection(varargin)
-    cval    = get(varargin{1}, 'value'); 
+    cval    = get(varargin{1}, 'value');
     h       = findall(varargin{3}, 'tag', 'opt');
     set(h, 'value', 0);
-    set(varargin{1}, 'value', cval); 
+    set(varargin{1}, 'value', cval);
     drawnow;
 end
 function mfile_showhelp(varargin)
